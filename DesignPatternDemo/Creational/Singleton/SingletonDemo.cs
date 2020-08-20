cbb0412e4593a458ed6005215045e88d741ed60d
kn52 <ashish52922@gmail.com>
2020-08-20 23:39:07 +0530

    [Aashish]ADDED:Singleton design pattern without thread safe

---
+++ DesignPatternDemo/Creational/Singleton/SingletonDemo.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignPatternDemo.Creational.Singleton
+{
+    using System;
+    public class SingletonDemo
+    {
+        public static void SFactory()
+        {
+            Console.WriteLine("=====Singleton Options=====");
+            Console.WriteLine();
+            Console.WriteLine("1. With out thread safe.");
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            Console.Clear();
+
+            switch (choice)
+            {
+                case "1":
+                    WithoutThreadSafe();
+                    break;
+            }            
+        }
+
+        private static void WithoutThreadSafe()
+        {
+            Console.WriteLine("=====Singleton Pattern Without Thread Safe=====");
+            SingletonWithoutThreadSafe.GetInstance.PrintDetails("from Teacher");
+            SingletonWithoutThreadSafe.GetInstance.PrintDetails("From Student");
+        }        
+    }
+}
---
+++ DesignPatternDemo/Creational/Singleton/SingletonWithoutThreadSafe.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPatternDemo.Creational.Singleton
+{
+    using System;
+    public class SingletonWithoutThreadSafe
+    {
+        private static int counter = 0;
+        private static SingletonWithoutThreadSafe instance = null;
+        public static SingletonWithoutThreadSafe GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SingletonWithoutThreadSafe();
+                return instance;
+            }
+
+        }
+
+        private SingletonWithoutThreadSafe() => Console.WriteLine("Counter Value " + (++counter).ToString());
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
