07678740da44d27cf12ff9e5cf62dd5c0d718753
kn52 <ashish52922@gmail.com>
2020-08-21 22:36:59 +0530

    [Aashish]ADDED:Singleton design pattern with lazy

--- DesignPatternDemo/Creational/Singleton/SingletonDemo.cs
+++ DesignPatternDemo/Creational/Singleton/SingletonDemo.cs
@@ -12,6 +12,7 @@
             Console.WriteLine("1. With out thread safe.");
             Console.WriteLine("2. With thread safe.");
             Console.WriteLine("3. With No Lazy.");
+            Console.WriteLine("4. With Lazy.");
             Console.Write("Enter your choice: ");
             string choice = Console.ReadLine();
 
@@ -29,6 +30,10 @@
 
                 case "3":
                     WithNoLazy();
+                    break;
+
+                case "4":
+                    WithLazy();
                     break;
             }            
         }
@@ -48,6 +53,13 @@
         private static void WithNoLazy()
         {
             Console.WriteLine("=====Singleton Pattern With No Lazy=====");
+            Parallel.Invoke(
+                () => SingletonWithNoLazy.GetInstance.PrintDetails("from Teacher"),
+                () => SingletonWithNoLazy.GetInstance.PrintDetails("From Student"));
+        }
+        private static void WithLazy()
+        {
+            Console.WriteLine("=====Singleton Pattern With Lazy=====");
             Parallel.Invoke(
                 () => SingletonWithNoLazy.GetInstance.PrintDetails("from Teacher"),
                 () => SingletonWithNoLazy.GetInstance.PrintDetails("From Student"));
---
+++ DesignPatternDemo/Creational/Singleton/SingletonWithLazy.cs
@@ -0,0 +1,23 @@
+﻿namespace DesignPatternDemo.Creational.Singleton
+{
+    using System;
+    public class SingletonWithLazy
+    {
+        private static int counter = 0;
+
+        private static readonly Lazy<SingletonWithLazy> Instancelock =
+                    new Lazy<SingletonWithLazy>(() => new SingletonWithLazy());
+        public static SingletonWithLazy GetInstance
+        {
+            get
+            {
+                return Instancelock.Value;
+            }
+        }
+        private SingletonWithLazy() => Console.WriteLine("Counter Value " + (++counter).ToString());
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
